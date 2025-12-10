a7bf253d331668be7e490d9d3f1f760c796d3d78
camerashrimp <731406@student.dusd.net>
2025-12-10 13:47:13 -0800

    addedgamecontrols
    10:15 timestamp a little more  left of video 6/10

--- LozanoFinalFlappyP6/Assets/Scripts/Bird.cs
+++ LozanoFinalFlappyP6/Assets/Scripts/Bird.cs
@@ -8,12 +8,14 @@ public class Bird : MonoBehaviour
 
     private bool isDead = false;
     private Rigidbody2D rb2d;
+    private Animator anim;
 
 
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -25,11 +27,14 @@ public class Bird : MonoBehaviour
             {
                 rb2d.velocity = Vector2.zero;
                 rb2d.AddForce(new Vector2(0, upforce));
+                anim.SetTrigger("flap");
             }
         }
     }
     void OnCollisionEnter2D ()
     {
         isDead = true;
+        anim.SetTrigger("die");
+        GameControl.instance.BirdDied();
     }
 }
